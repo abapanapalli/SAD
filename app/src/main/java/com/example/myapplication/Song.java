118e2da6b3b576ea04e44a2f41d7c524b169807c
abapanapalli <abapanapalli@wpi.edu>
2022-04-04 14:33:26 -0400

    Commented the code

--- app/src/main/java/com/example/myapplication/Song.java
+++ app/src/main/java/com/example/myapplication/Song.java
@@ -4,13 +4,15 @@ public class Song {
 
     // Data
 
-    private String title;
-    private String artist;
-    private double duration;
-    private double tempo;
-    private double valence;
+    private String title; // The title of the song
+    private String artist; // The artist which produced the song
+    private double duration; // How long the song runs for, measured in seconds
+    private double tempo; // The number of beats per minute (bpm)
+    private double valence; // Describes the musical positiveness conveyed by a track
 
     // Constructor
+
+    // Constructs a Song object which contains five parameters (title, artist, duration, tempo, and valence)
 
     public Song(String t, String a, double d, double bpm, double v) {
         title = t;
@@ -22,41 +24,91 @@ public class Song {
 
     // Methods
 
+    /**
+     * Method which gets the title of a song
+     * @return title The title of the song
+     */
+
     public String getTitle() {
         return title;
     }
+
+    /**
+     * Method which sets the title of a song
+     * @param title The new title of the song
+     */
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Method which gets the artist of a song
+     * @return artist The artist of the song
+     */
+
     public String getArtist() {
         return artist;
     }
+
+    /**
+     * Method which sets the artist of a song
+     * @param artist The new artist of the song
+     */
 
     public void setArtist(String artist) {
         this.artist = artist;
     }
 
+    /**
+     * Method which gets the duration of a song (measured in seconds)
+     * @return duration The duration of the song
+     */
+
     public double getDuration() {
         return duration;
     }
+
+    /**
+     * Method which sets the duration of a song
+     * @param duration The new duration of the song
+     */
 
     public void setDuration(double duration) {
         this.duration = duration;
     }
 
+    /**
+     * Method which gets the tempo of a song (measured in beats per minute - bpm)
+     * @return tempo The tempo of the song
+     */
+
     public double getTempo() {
         return tempo;
     }
+
+    /**
+     * Method which sets the tempo of a song
+     * @param tempo The new tempo of the song
+     */
 
     public void setTempo(double tempo) {
         this.tempo = tempo;
     }
 
+    /**
+     * Method which gets the valence of a song, measured on a scale from 0 to 1
+     * @return valence The valence of the song
+     */
+
     public double getValence() {
         return valence;
     }
+
+    /**
+     * Method which sets the valence of a song
+     * @param valence The new valence of the song
+     */
 
     public void setValence(double valence) {
         this.valence = valence;
