4eca1e0dcbbecd30e2f8973fe1cb8812bc2df7fb
abapanapalli <abapanapalli@wpi.edu>
2022-05-11 03:52:54 -0400

    finished some code, don't really know if it works, we can find out tomorrow. i think it works tho

--- app/src/main/java/com/example/myapplication/MainActivityPlaylists.java
+++ app/src/main/java/com/example/myapplication/MainActivityPlaylists.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivityPlaylists extends AppCompatActivity {
-Button backtohome;
+
+    Button backtohome;
+
+    RecyclerView recyclerView;
+    ArrayList<Playlist> playlistArrayList;
+    Adapter myAdapter;
+    String[] songName;
+    String[] songArtist;
+    int[] imageResourceId;
 
 
     @Override
@@ -25,6 +38,45 @@ Button backtohome;
                 Intent intent = new Intent(MainActivityPlaylists.this,MainActivityHomePage.class);
                 startActivity(intent);
             }
+        });
 
-    });
-}}
+        exercise.setSongs();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        playlistArrayList = new ArrayList<Playlist>();
+
+        myAdapter = new Adapter(this, playlistArrayList);
+        recyclerView.setAdapter(myAdapter);
+
+        songName = new String[exercise.getSongList().size()];
+        for (int i = 0; i < songName.length; i++) {
+            for (Song song: exercise.getSongList()) {
+                songName[i] = song.getTitle();
+            }
+        }
+
+        songArtist = new String[exercise.getSongList().size()];
+        for (int i = 0; i < songArtist.length; i++) {
+            for (Song song: exercise.getSongList()) {
+                songArtist[i] = song.getArtist();
+            }
+        }
+
+        imageResourceId = new int[exercise.getSongList().size()];
+        for (int i = 0; i < songName.length; i++) {
+            imageResourceId[i] = R.drawable.personalrecordlogo3;
+        }
+
+        getData();
+    }
+
+    private void getData() {
+        for(int i = 0; i < songName.length; i++) {
+            Playlist song = new Playlist(songName[i], songArtist[i], imageResourceId[i]);
+            playlistArrayList.add(song);
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/Playlist.java
+++ app/src/main/java/com/example/myapplication/Playlist.java
@@ -6,7 +6,7 @@ public class Playlist {
     String artist;
     int titleImage;
 
-    public Playlist(String name, int titleImage) {
+    public Playlist(String name, String artist, int titleImage) {
         this.name = name;
         this.artist = artist;
         this.titleImage = titleImage;
